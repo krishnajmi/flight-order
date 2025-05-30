23693f8e8aa79cf2ef2b398005c600e190087c9c
krishna pandey <109947494+krishnajmi@users.noreply.github.com>
2025-05-30 11:22:22 +0400

    error msg added for invalid journey

--- flight/controller.go
+++ flight/controller.go
@@ -24,7 +24,7 @@ func (fc *FlightController) GetFlightOrderView(c echo.Context) error {
 	var flightOrderRequest FlightOrderRequest
 	if err := c.Bind(&flightOrderRequest); err != nil {
 		slog.Error("invalid request format", "error", err)
-		return c.JSON(http.StatusBadRequest, map[string]string{
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
 			"error": "Invalid request format",
 		})
 	}
@@ -32,8 +32,9 @@ func (fc *FlightController) GetFlightOrderView(c echo.Context) error {
 	journeyOrder, err := fc.FlightJourneyService.GetJourneyOrder(c.Request().Context(), flightOrderRequest.Journies)
 	if err != nil {
 		slog.Error("error getting journey order", "error", err)
-		return c.JSON(http.StatusInternalServerError, map[string]string{
-			"error": "Internal server error",
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"error":     "Internal server error",
+			"error_msg": err.Error(),
 		})
 	}
 
